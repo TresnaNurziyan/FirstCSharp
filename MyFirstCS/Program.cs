5cb5cf489666286bf3fc42f3791fd9f32eadfcd8
Tresna Nurziyan <tresna.nurziyan@gmail.com>
2017-07-26 14:27:47 +0700

    First c sharp

---
+++ MyFirstCS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nama;
+            String alamat;
+            int umur;
+            String goldar;
+
+            Console.Write("Masukan nama             : ");
+            nama = Console.ReadLine();
+            Console.Write("Masukan alamat           : ");
+            alamat = Console.ReadLine();
+            Console.Write("Masukan umur             : ");
+            umur = int.Parse(Console.ReadLine());
+            Console.Write("Masukan golongan darah   : ");
+            goldar = Console.ReadLine();
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Nama anda            : " + nama);
+            Console.WriteLine("Alamat anda          : " + alamat);
+            Console.WriteLine("Umur anda            : " + umur);
+            Console.WriteLine("Golongan darah anda  : " + goldar);
+            Console.WriteLine("--------------------------------------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
